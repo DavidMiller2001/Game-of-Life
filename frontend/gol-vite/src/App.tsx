9a1d617a69d82c590f952ab216831c5ec81048c6
David Miller <davidwaynemiller2001@gmail.com>
2025-07-20 11:04:38 -0400

    add shadcn and start implementing sidebar

--- frontend/gol-vite/src/App.tsx
+++ frontend/gol-vite/src/App.tsx
@@ -1,8 +1,14 @@
 import { useState } from 'react';
+import {
+  SidebarProvider,
+  SidebarTrigger,
+  useSidebar,
+} from './components/ui/sidebar';
+import AppSidebar from './components/custom-components/AppSidebar';
 
 function App() {
-  const [rows, setRows] = useState(8);
-  const [cols, setCols] = useState(8);
+  const [rows, setRows] = useState(5);
+  const [cols, setCols] = useState(5);
 
   const root = document.documentElement;
   root.style.setProperty('--col-number', `${cols}`);
@@ -12,24 +18,23 @@ function App() {
   const [grid, setGrid] = useState(tempGrid);
 
   return (
-    <div className='bg-gray-800 flex items-center justify-center flex-col h-screen'>
-      <h1 className='text-white text-5xl py-4'>Game of Life</h1>
-      <main className='max-w-[1000px] w-full flex justify-between items-center'>
-        <div id='grid-parent' className='flex-1/2'>
-          {grid.map((row) =>
-            row.map((cell, i) => <Cell key={i} status={cell} />)
-          )}
+    <SidebarProvider>
+      <AppSidebar />
+      <div className='bg-gray-800 flex items-center w-full justify-center flex-col min-h-screen'>
+        <div className='flex items-center'>
+          <h1 className='text-white text-5xl p-4'>Game of Life</h1>
+          <SidebarTrigger className='text-white' />
+          <SettingsButton />
         </div>
-        <div className='h-full flex-1/2 p-8'>
-          <form className='bg-gray-600 w-full h-full rounded-lg p-8 text-white'>
-            <div className='flex gap-2'>
-              <label htmlFor='cols'>Number of Columns</label>
-              <input type='text' />
-            </div>
-          </form>
-        </div>
-      </main>
-    </div>
+        <main className='max-w-[1024px] w-full flex flex-col justify-center items-center'>
+          <div className='w-[50%] ' id='grid-parent'>
+            {grid.map((row) =>
+              row.map((cell, i) => <Cell key={i} status={cell} />)
+            )}
+          </div>
+        </main>
+      </div>
+    </SidebarProvider>
   );
 }
 
@@ -41,11 +46,16 @@ function Cell(props: { status: number }) {
       onClick={() => {
         setIsAlive(!isAlive);
       }}
-      className={`bg-slate-600 border-1 aspect-square border-black ${
+      className={`bg-slate-600 border-1 h-full w-full aspect-square border-black ${
         isAlive && 'bg-white'
       }`}
-    ></div>
+    />
   );
+}
+
+function SettingsButton() {
+  const { toggleSidebar } = useSidebar();
+  return <button onClick={toggleSidebar}>Settings</button>;
 }
 
 export default App;
