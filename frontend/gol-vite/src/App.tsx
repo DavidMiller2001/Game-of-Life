e0cc2100feb4c45a1bde8881683a3ff2ced19f63
David Miller <davidwaynemiller2001@gmail.com>
2025-07-20 02:10:07 -0400

    resize grid to make room for form

--- frontend/gol-vite/src/App.tsx
+++ frontend/gol-vite/src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 
 function App() {
-  const [rows, setRows] = useState(10);
-  const [cols, setCols] = useState(10);
+  const [rows, setRows] = useState(8);
+  const [cols, setCols] = useState(8);
 
   const root = document.documentElement;
   root.style.setProperty('--col-number', `${cols}`);
@@ -12,13 +12,24 @@ function App() {
   const [grid, setGrid] = useState(tempGrid);
 
   return (
-    <main className='h-screen flex bg-gray-800 justify-center items-center'>
-      <div id='grid-parent'>
-        {grid.map((row) =>
-          row.map((cell, i) => <Cell key={i} status={cell} />)
-        )}
-      </div>
-    </main>
+    <div className='bg-gray-800 flex items-center justify-center flex-col h-screen'>
+      <h1 className='text-white text-5xl py-4'>Game of Life</h1>
+      <main className='max-w-[1000px] w-full flex justify-between items-center'>
+        <div id='grid-parent' className='flex-1/2'>
+          {grid.map((row) =>
+            row.map((cell, i) => <Cell key={i} status={cell} />)
+          )}
+        </div>
+        <div className='h-full flex-1/2 p-8'>
+          <form className='bg-gray-600 w-full h-full rounded-lg p-8 text-white'>
+            <div className='flex gap-2'>
+              <label htmlFor='cols'>Number of Columns</label>
+              <input type='text' />
+            </div>
+          </form>
+        </div>
+      </main>
+    </div>
   );
 }
 
@@ -30,7 +41,7 @@ function Cell(props: { status: number }) {
       onClick={() => {
         setIsAlive(!isAlive);
       }}
-      className={`bg-slate-600 h-[50px] w-[50px] border-1 border-black ${
+      className={`bg-slate-600 border-1 aspect-square border-black ${
         isAlive && 'bg-white'
       }`}
     ></div>
